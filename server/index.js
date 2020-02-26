6169ed647c1fd8242eb5f9b86c8766c4a9952630
manoj07thapa <mnjinvincible@gmail.com>
2020-02-26 13:03:10 +0545

    moved private info to sonfig files

--- server/index.js
+++ server/index.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const app = express();
-
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const { User } = require('./models/user');
+const config = require('../config/key');
 
 mongoose
-	.connect('mongodb://manoj:12oclock_007@ds239157.mlab.com:39157/shopping-cart', {
+	.connect(config.mongoURI, {
 		useNewUrlParser: true,
 		useCreateIndex: true,
 		useUnifiedTopology: true
 	})
 	.then(() => console.log('db connected'))
 	.catch((err) => console.log(err));
+app.use(express.json());
+app.use(cookieParser());
+
+app.post('/api/users/resister', (req, res) => {
+	const user = new User(req.body);
+	user.save((err, userData) => {
+		if (err) {
+			return res.json({ success: false, err });
+		}
+		return res.status(200).json({ success: true });
+	});
+});
 
 app.get('/', (req, res) => {
 	res.send('Hello from node');
